6bb47049a6831b37f17a99e71b511530f3883e8c
excelion29 <wronkowcanolt@gmail.com>
2022-07-15 18:00:17 -0500

    cambios 18:08

--- apps/static/assets/js/FLSCD/PDF_read.js
+++ apps/static/assets/js/FLSCD/PDF_read.js
@@ -12,17 +12,30 @@ $(function(){
             contentType: false,
 	        processData: false,
             success: function (data){
-                collect_context = data.Lecturas.context.map(function(value){
-                    return value.split("\n");
-                });
-                console.log(collect_context);
-                $("#title").html("Title: " + data.Lecturas.title);
-                $("#metadata").html("Metadata: " + data.Lecturas.Metadada.producer);
-                $("#context").html("Orden de Trabajo: " +collect_context[0][7]);
+                data_pdf(data);
             },
             error: function(textStatus) { 
                 alert("Status: " + textStatus);
             },
         });   
     });
+
+    function data_pdf(attributes){
+        collect_context = attributes.Lecturas.context.map(function(value){
+            return value.split("\n");
+        });
+        
+
+        container = document.getElementById("container_PDF");
+        var card_mb4 = document.createElement('div');
+        card_mb4.setAttribute('class','card mb-4');
+        container.appendChild(card_mb4);
+        var card_header = document.createElement('div');
+        card_header.setAttribute('class','card-header pb-0');
+        card_mb4.appendChild(card_header);
+        var h6 = document.createElement('h6');
+        h6.appendChild(document.createTextNode(attributes.Lecturas.title));
+        card_header.appendChild(h6);
+        container.appendChild(card_mb4);
+    }
 });
